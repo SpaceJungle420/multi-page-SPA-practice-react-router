95d9926882d38087e699dc94ac0664504c471672
SpaceJungle420 <46966551+SpaceJungle420@users.noreply.github.com>
2023-06-02 09:35:07 -0300

    error handline with custom errors

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -3,6 +3,11 @@ import { useLoaderData, useRouteLoaderData } from "react-router-dom";
 
 function EventsPage() {
   const data = useLoaderData();
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
+
   const events = data.events;
 
   return (
@@ -18,7 +23,8 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events" };
+    throw { message: "Could not fetch events." };
   } else {
     const resData = await response.json();
     return response;
